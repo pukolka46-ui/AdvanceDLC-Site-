e946f236cdfa95c3729e47de26d9ce81ceace060
pukolka46-ui <pukolka46@gmail.com>
2026-01-26 18:54:02 +0300

    Фикс Регистрации

--- lib/mailer.ts
+++ lib/mailer.ts
@@ -1,10 +1,11 @@
-import nodemailer from "nodemailer";
+// lib/mailer.ts
+const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
-    user: process.env.EMAIL_USER,   // сюда Vercel автоматически подставит EMAIL_USER
-    pass: process.env.EMAIL_PASS,   // сюда Vercel автоматически подставит EMAIL_PASS
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
   },
 });
 
